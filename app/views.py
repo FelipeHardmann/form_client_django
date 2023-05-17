b95ea1586bfbddf1f04ab746326686e65d52b9c7
FelipeHardmann <fashardmann@gmail.com>
2023-05-17 10:58:51 -0300

    Add comments

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
+'''
+    Arquivo responsável por intermediar o front com o banco de dados
+    recebendo usuários e fazendo suas regras de negócio.
+'''
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DetailView, DeleteView
 from .models import Cliente
 
 class ClienteCreateView(CreateView):
+    '''
+        Classe para criação da View
+        Aqui criamos nossa visualização e comunicação para o front-end
+    '''
     model = Cliente
     fields = '__all__'
     template_name = 'form_cliente.html'
@@ -11,17 +20,27 @@ class ClienteCreateView(CreateView):
  
 
 class ClienteListView(ListView):
+    '''
+        Classe para enviar informações de todos os clientes
+        listando eles. 
+    '''
     model = Cliente
     template_name = 'lista_clientes.html'
 
 
 class ClienteDetailView(DetailView):
+    '''
+        Classe que vai detalhar somente um único cliente
+    '''
     model = Cliente
     template_name = 'lista_cliente.html'
     context_object_name = 'cliente'
 
 
 class ClienteUpdateView(UpdateView):
+    '''
+        Classe para atualizar dados de um cliente
+    '''
     model = Cliente
     fields = '__all__'
     template_name = 'form_cliente.html'
@@ -29,6 +48,9 @@ class ClienteUpdateView(UpdateView):
 
 
 class ClienteDeleteView(DeleteView):
+    '''
+        Classe para deletar clientes
+    '''
     model = Cliente
     template_name = 'remover_cliente.html'
     success_url = reverse_lazy('lista_clientes') 
