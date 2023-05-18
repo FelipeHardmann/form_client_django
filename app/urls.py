8642e7b853b94fd1fb7cd0e818c217d39b7d6a9c
FelipeHardmann <fashardmann@gmail.com>
2023-05-17 21:27:37 -0300

    Add conditionals

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from .views import ClienteCreateView, ClienteListView, ClienteUpdateView, ClienteDetailView, ClienteDeleteView
 
 urlpatterns = [
-    path('form_cliente', ClienteCreateView.as_view()),
+    path('form_cliente', ClienteCreateView.as_view(), name='cadastrar_cliente'),
     path('lista_clientes', ClienteListView.as_view(), name='lista_clientes'),
     path('form_cliente/<int:pk>', ClienteUpdateView.as_view(), name='editar_cliente'),
     path('lista_cliente/<int:pk>', ClienteDetailView.as_view(), name='lista_cliente'),
