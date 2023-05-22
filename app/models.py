6f6bc8271c7552f2555ea89b2a7da97b8a9d9827
FelipeHardmann <fashardmann@gmail.com>
2023-05-22 15:38:15 -0300

    Add comments

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Cliente(models.Model):
+    '''
+        Classe referente a tabela de pessoa do banco de dados
+    '''
     nome = models.CharField(max_length=255, null=False, blank=False)
     data_nasc = models.DateField(null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
--- app/templatetags/meus_filtros.py
+++ app/templatetags/meus_filtros.py
@@ -9,4 +9,7 @@ register = template.Library()
 
 @register.filter(name="remover_caracter")
 def remover_caracter(var, caracter):
+    '''
+        Função de tamplate tag com o objetivo de retirar a letra que o programador desejar
+    '''
     return var.replace(caracter, '')
